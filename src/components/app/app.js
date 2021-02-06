6d766655312f53ef4a8751b0782612c5604461fe
duho3abp <DanZhidkof@yandex.ru>
2021-02-06 15:17:03 +0300

    create todo delete functionally

--- src/components/app/app.js
+++ src/components/app/app.js
@@ -1,4 +1,4 @@
-import React from 'react';
+import React, { Component } from 'react';
 import './app.css'
 
 import AppHeader from '../app-header';
@@ -6,22 +6,35 @@ import TodoList from '../todo-list';
 import SearchPanel from '../search-panel';
 import ItemStatusFilter from '../item-status-filter';
 
-const App = () => {
-    const todoData = [
-        { value: 'Learn React', important: false, id: 1 },
-        { value: 'Build awesome App', important: true, id: 2 },
-        { value: 'Drink coffee', important: false, id: 3 },
-        { value: 'I need to develop a lot of components in Ract. I am very happy!', important: false, id: 4 }
-    ];
+export default class App extends Component {
+    state = {
+        todoData: [
+            { value: 'Learn React', important: false, id: 1 },
+            { value: 'Build awesome App', important: true, id: 2 },
+            { value: 'Drink coffee', important: false, id: 3 },
+            { value: 'I need to develop a lot of components in Ract. I am very happy!', important: false, id: 4 }
+        ]
+    }
 
-    return (
-      <div className="app">
-        <AppHeader />
-        <SearchPanel />
-        <ItemStatusFilter />
-        <TodoList todos={ todoData } />
-      </div>
-    );
-};
+    deleteTodo = id => {
+        const { todoData } = this.state;
+        const newTodoData = todoData.filter(obj => obj.id !== id);
+        this.setState({ todoData: newTodoData });
+    }
 
-export default App;
+    render() {
+        const { todoData } = this.state;
+
+        return (
+            <div className="app">
+              <AppHeader />
+              <SearchPanel />
+              <ItemStatusFilter />
+
+              <TodoList 
+                todos={ todoData } 
+                deleteTodo={ this.deleteTodo } />
+            </div>
+          );
+    }
+}
--- src/components/todo-list/todo-list.js
+++ src/components/todo-list/todo-list.js
@@ -3,10 +3,11 @@ import './todo-list.css';
 
 import TodoListItem from '../todo-list-item';
 
-const TodoList = ({ todos }) => {
+const TodoList = ({ todos, deleteTodo }) => {
     const elements = todos.map(({ id, ...item }) => {
         return (
             <TodoListItem 
+                deleteTodo={ () => deleteTodo(id) }
                 key={ id }
                 { ...item }  />
         );
